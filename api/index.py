8c9f35e32fedb7f64b369867b3927e1177bf16b2
Ali Anwar <greenywish@gmail.com>
2025-03-01 18:23:31 +0530

    Basic Flask template

--- api/index.py
+++ api/index.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! Flask API on Vercel."
+    return jsonify({"message": "Hello, World! Flask API on Vercel."})
 
-# Required for Vercel
+# Required for Vercel serverless function
 def handler(event, context):
     return app(event, context)
 
