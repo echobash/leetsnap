b5ad54f5e6b31427d6f683130af62483c4cdf21e
Ali Anwar <greenywish@gmail.com>
2025-03-01 19:00:25 +0530

    Basic Flask template

--- api/index.py
+++ api/index.py
@@ -5,7 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, World! Welcome to Vercel with Python!"
-
-# Required for Vercel (acts as entry point)
-def handler(event, context):
-    return app(event, context)
